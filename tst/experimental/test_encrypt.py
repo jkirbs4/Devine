8959d3fe9f96fc2386f1abe4f9c2fc863557483e
Josh Kirby <122172835+jkirbs4@users.noreply.github.com>
2026-01-28 10:38:52 -0500

    Completed cipher example

--- tst/experimental/test_encrypt.py
+++ tst/experimental/test_encrypt.py
@@ -4,20 +4,43 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 class Cipher:
 
-    def __init__(self, bits: int = 12, associated_data: str = ""):
+    def __init__(self, bits: int = 256, associated_data: str = "", nonce: int = 12):
 
-        self.nonce = os.urandom(12) # Must be unique per encryption key.
+        self.nonce = os.urandom(nonce) # Must be unique per encryption key.
         self.associated_data = associated_data
-        self.cipher = AESGCM(self.generate_key(bits))
+        self.cipher = AESGCM(self.keygen(bits))
 
+    @staticmethod
     def keygen(bits: int = 256):
         return AESGCM.generate_key(bit_length=bits)
 
-    def encrypt(self, message: str)
+    def encrypt(self, message: str):
         
-        return self.cipher.encrypt(self.nonce, message, self.associated_data)
+        message_bytes = message.encode("utf-8")
+        associated_data_bytes = self.associated_data.encode("utf-8")
+
+        return self.cipher.encrypt(self.nonce, message_bytes, associated_data_bytes)
     
     def decrypt(self, ciphertext: str):
 
-        return self.cipher.decrypt(self.nonce, ciphertext, self.associated_data)
+        associated_data_bytes = self.associated_data.encode("utf-8")
+        message_bytes = self.cipher.decrypt(self.nonce, ciphertext, associated_data_bytes)
+        message = message_bytes.decode("utf-8")
 
+        return message
+    
+def main():
+
+    cipher = Cipher()
+    message: str = "This is my message."
+    print("Message: ", message)
+    ciphertext: str = cipher.encrypt(message)
+    print("Ciphertext: ", ciphertext)
+    reconstructed_message = cipher.decrypt(ciphertext)
+    print("Reconstructed Message: ", reconstructed_message)
+
+    assert (message == reconstructed_message)
+    print("The messages match!")
+
+if __name__ == "__main__":
+    main()
